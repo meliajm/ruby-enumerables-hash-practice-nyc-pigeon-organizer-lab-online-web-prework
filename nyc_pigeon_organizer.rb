ee76e1ae687e3cdd7e9e49e88135c5619c6ba4cb
melia miller <meliajm@gmail.com>
2019-11-30 17:42:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |new_value, names|
       names.each do |name|
-        # binding.pry
+        binding.pry
         if !new_hash[name]
           new_hash[name] = {}
         end
